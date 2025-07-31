4f1d4049dd98713f79b699aaaa3cd582b839fad4
Đăng Khoa <155549520+dangkhoa1102@users.noreply.github.com>
2025-07-31 14:47:35 +0700

    debug, obj file

--- HYDRA.GUI/obj/Debug/net9.0-windows/HYDRA.GUI.AssemblyInfo.cs
+++ HYDRA.GUI/obj/Debug/net9.0-windows/HYDRA.GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HYDRA.GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e05c0d35d8347baa525a4b08f991e0a4d8146f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1030384d2f678a3c4dd7427959ec76474bc9bd85")]
 [assembly: System.Reflection.AssemblyProductAttribute("HYDRA.GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HYDRA.GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
